7f198dd74dbbe1bd171d88e0c15035cbe9b26dcb
jackmwhit <jack.whitworth@substrakt.com>
2024-02-14 10:44:00 +0000

    v1.2.4 rebuild

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PrismPress
 
-Version: `1.2.3`
+Version: `1.2.4`
 
 PrismPress is a WordPress plugin that adds a new Gutenberg block for display syntax-highlighted code powered by [Prism.js](https://prismjs.com/index.html).
 
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'b516be2cd774d89693f2');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'cf2206fdb0fb05c241dd');
